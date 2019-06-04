f7377302ea843dd871df242e591cda3d37c938c0
Joffrey BION <joffrey.bion@gmail.com>
2019-06-05 00:43:08 +0200

    Fix spring dependencies

--- krossbow-engine-spring/build.gradle.kts
+++ krossbow-engine-spring/build.gradle.kts
@@ -1,13 +1,17 @@
 plugins {
     kotlin("jvm")
+    id("org.springframework.boot") version "2.1.5.RELEASE"
 }
+
+apply(plugin = "io.spring.dependency-management")
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":krossbow-engine-api"))
-    implementation("org.springframework:spring-messaging:4.3.8.RELEASE")
-    implementation("org.springframework:spring-websocket:4.3.8.RELEASE")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
+
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework:spring-messaging")
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
