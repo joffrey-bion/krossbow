fbc7ddfc59bdc79ddcfd3c317c3132034af28cc6
jbion <joffrey.bion@amadeus.com>
2019-06-05 22:07:36 +0200

    Fix Spring StompSessionHandler (too many continuations)

--- krossbow-engine-spring/src/main/kotlin/org/hildan/krossbow/engines/spring/SpringKrossbowEngine.kt
+++ krossbow-engine-spring/src/main/kotlin/org/hildan/krossbow/engines/spring/SpringKrossbowEngine.kt
@@ -7,10 +7,7 @@ import org.hildan.krossbow.engines.KrossbowConfig
 import org.hildan.krossbow.engines.KrossbowEngine
 import org.hildan.krossbow.engines.KrossbowSession
 import org.springframework.messaging.converter.MappingJackson2MessageConverter
-import org.springframework.messaging.simp.stomp.StompCommand
-import org.springframework.messaging.simp.stomp.StompHeaders
-import org.springframework.messaging.simp.stomp.StompSession
-import org.springframework.messaging.simp.stomp.StompSessionHandler
+import org.springframework.messaging.simp.stomp.*
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.web.socket.client.WebSocketClient
 import org.springframework.web.socket.client.standard.StandardWebSocketClient
@@ -47,47 +44,29 @@ class SpringKrossbowClient(private val client: WebSocketStompClient): KrossbowCl
 
     override suspend fun connect(url: String, login: String?, passcode: String?): KrossbowSession {
         return suspendCoroutine { cont ->
-            val futureSession = client.connect(url, object : StompSessionHandler {
-                override fun handleException(
-                    session: StompSession?,
-                    command: StompCommand?,
-                    headers: StompHeaders?,
-                    payload: ByteArray?,
-                    exception: Throwable?
-                ) {
-                    cont.resumeWithException(exception!!)
-                }
-
-                override fun handleTransportError(session: StompSession?, exception: Throwable?) {
-                    cont.resumeWithException(exception!!)
-                }
-
-                override fun handleFrame(headers: StompHeaders?, payload: Any?): Unit = TODO("not implemented")
-
-                override fun afterConnected(session: StompSession?, connectedHeaders: StompHeaders?): Unit = TODO("not implemented")
-
-                override fun getPayloadType(headers: StompHeaders?): Type = TODO("not implemented")
-            })
+            val futureSession = client.connect(url, NoopStompSessionHandler)
             cont.resume(SpringKrossbowSession(futureSession.get()))
         }
     }
 }
 
+private object NoopStompSessionHandler: StompSessionHandlerAdapter()
+
 private class SpringKrossbowSession(private val session: StompSession): KrossbowSession {
 
     override suspend fun send(destination: String, body: Any) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("send not implemented")
     }
 
     override suspend fun <T> subscribe(destination: String, onFrameReceived: (T) -> Unit) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("subscribe not implemented")
     }
 
     override suspend fun <T> CoroutineScope.subscribe(destination: String): ReceiveChannel<T> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("subscribe (channel) not implemented")
     }
 
     override suspend fun disconnect() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("disconnect not implemented")
     }
 }
