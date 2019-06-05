6d647048b629e23e25708d9e5687fe27236b315c
jbion <joffrey.bion@amadeus.com>
2019-06-05 22:08:55 +0200

    Fix Spring websocket and Jackson dependencies

--- krossbow-engine-spring/build.gradle.kts
+++ krossbow-engine-spring/build.gradle.kts
@@ -1,17 +1,25 @@
 plugins {
     kotlin("jvm")
-    id("org.springframework.boot") version "2.1.5.RELEASE"
 }
 
-apply(plugin = "io.spring.dependency-management")
-
 dependencies {
+    api(project(":krossbow-engine-api"))
+
     implementation(kotlin("stdlib-jdk8"))
-    implementation(project(":krossbow-engine-api"))
+    implementation(kotlin("reflect")) // required by jackson-module-kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
 
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("org.springframework:spring-messaging")
+    // For Spring's STOMP client
+    val springVersion = "5.1.7.RELEASE"
+    implementation("org.springframework:spring-websocket:$springVersion")
+    implementation("org.springframework:spring-messaging:$springVersion")
+
+    // JSR 356 - Java API for WebSocket (reference implementation)
+    // Low-level mplementation required by Spring's client
+    implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client-jdk:1.15")
+
+    implementation("com.fasterxml.jackson.core:jackson-core:2.9.9")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.9")
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
