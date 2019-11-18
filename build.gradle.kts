b6882774dda10f2bb5265572ea27f745c62eae30
joffrey-bion <joffrey.bion@gmail.com>
2019-11-19 00:05:27 +0100

    Upgrade ktlint plugin to 9.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("js") version kotlinVersion apply false
     kotlin("multiplatform") version kotlinVersion apply false
     kotlin("plugin.spring") version kotlinVersion apply false
-    id("org.jlleitschuh.gradle.ktlint") version "7.1.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "9.1.1" apply false
     id("org.jetbrains.dokka") version "0.9.18" apply false
     id("com.jfrog.bintray") version "1.8.4" apply false
     id("org.hildan.github.changelog") version "0.8.0"
@@ -41,6 +41,13 @@ subprojects {
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    // The import ordering expected by ktlint is alphabetical, which doesn't match IDEA's formatter.
+    // Since it is not configurable, we have to disable the rule.
+    // https://github.com/pinterest/ktlint/issues/527
+    extensions.configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        disabledRules.set(setOf("import-ordering"))
     }
 
     afterEvaluate {
