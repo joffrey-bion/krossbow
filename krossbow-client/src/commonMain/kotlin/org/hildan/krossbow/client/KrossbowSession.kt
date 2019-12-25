53f1860e351e49b49fbfe6ca3ef67c5de3f52958
joffrey-bion <joffrey.bion@gmail.com>
2019-12-25 21:37:42 +0100

    Allow nullable payloads in send()

--- krossbow-client/src/commonMain/kotlin/org/hildan/krossbow/client/KrossbowSession.kt
+++ krossbow-client/src/commonMain/kotlin/org/hildan/krossbow/client/KrossbowSession.kt
@@ -34,11 +34,11 @@ class KrossbowSession(
      *
      * If auto-receipt is enabled, this method suspends until a RECEIPT frame is received from the server and returns a
      * [KrossbowReceipt]. If no RECEIPT frame is received from the server in the configured time limit, a
-     * [LostReceiptException] is thrown.
+     * [LostReceiptException][org.hildan.krossbow.engines.LostReceiptException] is thrown.
      *
      * If receipts are not enabled, this method sends the frame and immediately returns null.
      */
-    suspend inline fun <reified T : Any> send(destination: String, payload: T): KrossbowReceipt? =
+    suspend inline fun <reified T : Any> send(destination: String, payload: T?): KrossbowReceipt? =
             send(destination, payload, T::class)
 
     /**
@@ -46,7 +46,7 @@ class KrossbowSession(
      *
      * If auto-receipt is enabled, this method suspends until a RECEIPT frame is received from the server and returns a
      * [KrossbowReceipt]. If no RECEIPT frame is received from the server in the configured time limit, a
-     * [LostReceiptException] is thrown.
+     * [LostReceiptException][org.hildan.krossbow.engines.LostReceiptException] is thrown.
      *
      * If receipts are not enabled, this method sends the frame and immediately returns null.
      */
@@ -57,7 +57,7 @@ class KrossbowSession(
      *
      * If auto-receipt is enabled, this method suspends until a RECEIPT frame is received from the server and returns a
      * [KrossbowReceipt]. If no RECEIPT frame is received from the server in the configured time limit, a
-     * [LostReceiptException] is thrown.
+     * [LostReceiptException][org.hildan.krossbow.engines.LostReceiptException] is thrown.
      *
      * If receipts are not enabled, this method sends the frame and immediately returns null.
      */
