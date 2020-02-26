518509bf80898a3d567848dd3d4bd3431ae0e66a
Joffrey Bion <joffrey.bion@booking.com>
2020-02-26 23:37:07 +0100

    Improve output for tests that timeout

--- krossbow-client/src/commonTest/kotlin/org/hildan/krossbow/test/TestUtils.kt
+++ krossbow-client/src/commonTest/kotlin/org/hildan/krossbow/test/TestUtils.kt
@@ -1,12 +1,15 @@
 package org.hildan.krossbow.test
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.test.assertNotNull
 
 fun <T> runAsyncTestWithTimeout(millis: Long = 1000, block: suspend CoroutineScope.() -> T) = runAsyncTest {
-    withTimeout(millis) {
+    val result = withTimeoutOrNull(millis) {
         block()
+        Unit
     }
+    assertNotNull(result, "this test should run in less than ${millis}ms")
 }
 
 expect fun <T> runAsyncTest(block: suspend CoroutineScope.() -> T)
