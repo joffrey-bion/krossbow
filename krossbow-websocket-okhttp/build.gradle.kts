4d447f0a6ffd62a0020b7bfb41a10b67e63c6065
Joffrey Bion <joffrey.bion@booking.com>
2020-04-11 13:04:01 +0200

    Add OkHttpWebSocketClient
    Resolves:
    https://github.com/joffrey-bion/krossbow/issues/40

---
+++ krossbow-websocket-okhttp/build.gradle.kts
@@ -0,0 +1,50 @@
+import java.net.URL
+
+plugins {
+    kotlin("jvm")
+}
+
+description = "A Krossbow adapter for OkHttp's WebSocket client"
+
+dependencies {
+    api(project(":krossbow-websocket-api"))
+
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.3.3")
+
+    api("com.squareup.okhttp3:okhttp:4.5.0")
+}
+
+tasks.dokka {
+    dependsOn(":krossbow-websocket-api:dokka")
+    configuration {
+        externalDocumentationLink {
+            url = URL("file://${project(":krossbow-websocket-api").buildDir}/dokka/krossbow-websocket-api/")
+            packageListUrl = URL(url, "package-list")
+        }
+    }
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokka)
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["kotlin"])
+            artifact(dokkaJar)
+            artifact(sourcesJar)
+        }
+    }
+}
---
+++ krossbow-websocket-okhttp/src/main/kotlin/org/hildan/krossbow/websocket/okhttp/OkHttpWebSocketClient.kt
@@ -0,0 +1,86 @@
+package org.hildan.krossbow.websocket.okhttp
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import okio.ByteString
+import okio.ByteString.Companion.toByteString
+import org.hildan.krossbow.websocket.WebSocketFrame
+import org.hildan.krossbow.websocket.WebSocketListenerChannelAdapter
+import kotlin.coroutines.CoroutineContext
+import org.hildan.krossbow.websocket.WebSocketClient as KrossbowWebSocketClient
+import org.hildan.krossbow.websocket.WebSocketSession as KrossbowWebSocketSession
+
+class OkHttpWebSocketClient(
+    private val client: OkHttpClient = OkHttpClient()
+) : KrossbowWebSocketClient {
+
+    override suspend fun connect(url: String): KrossbowWebSocketSession {
+        val request = Request.Builder().url(url).build()
+        val listener = KrossbowToOkHttpListenerAdapter()
+        val okWebsocket = withContext(Dispatchers.IO) { client.newWebSocket(request, listener) }
+        return OkHttpSocketToKrossbowSessionAdapter(okWebsocket, listener)
+    }
+}
+
+class KrossbowToOkHttpListenerAdapter : WebSocketListener(), CoroutineScope {
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job
+
+    val channelListener: WebSocketListenerChannelAdapter = WebSocketListenerChannelAdapter()
+
+    override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+        launch { channelListener.onBinaryMessage(bytes.toByteArray()) }
+    }
+
+    override fun onMessage(webSocket: WebSocket, text: String) {
+        launch { channelListener.onTextMessage(text) }
+    }
+
+    override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+        launch { channelListener.onClose(code, reason) }
+        job.cancel()
+    }
+
+    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+        channelListener.onError(t)
+        job.cancel()
+    }
+
+    suspend fun stop() {
+        job.cancelAndJoin()
+    }
+}
+
+class OkHttpSocketToKrossbowSessionAdapter(
+    private val okSocket: WebSocket,
+    private val listener: KrossbowToOkHttpListenerAdapter
+) : KrossbowWebSocketSession {
+
+    override val incomingFrames: ReceiveChannel<WebSocketFrame> = listener.channelListener.incomingFrames
+
+    override suspend fun sendText(frameText: String) {
+        okSocket.send(frameText)
+    }
+
+    override suspend fun sendBinary(frameData: ByteArray) {
+        okSocket.send(frameData.toByteString())
+    }
+
+    override suspend fun close(code: Int, reason: String?) {
+        okSocket.close(code, reason)
+        listener.stop()
+    }
+}
