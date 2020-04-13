0a2eee72175c0b38aa793cc0b5ef330b29cbbe98
Joffrey Bion <joffrey.bion@booking.com>
2020-04-13 12:54:34 +0200

    Add tests for partial messages in WebSocketChannel

--- krossbow-websocket-core/src/commonTest/kotlin/org/hildan/krossbow/websocket/WebSocketChannelTest.kt
+++ krossbow-websocket-core/src/commonTest/kotlin/org/hildan/krossbow/websocket/WebSocketChannelTest.kt
@@ -20,6 +20,33 @@ class WebSocketChannelTest {
     }
 
     @Test
+    fun onTextMessage_partialMessages() = runSuspendingTest {
+        val adapter = WebSocketListenerChannelAdapter()
+        launch {
+            adapter.onTextMessage("complete", isLast = true)
+
+            adapter.onTextMessage("begin", isLast = false)
+            adapter.onTextMessage("-end", isLast = true)
+
+            adapter.onTextMessage("complete2", isLast = true)
+
+            adapter.onTextMessage("1", isLast = false)
+            adapter.onTextMessage("2", isLast = false)
+            adapter.onTextMessage("3", isLast = true)
+        }
+        assertEquals("complete", adapter.receiveText())
+        assertEquals("begin-end", adapter.receiveText(), "the complete msg should be sent when last part is received")
+        assertEquals("complete2", adapter.receiveText())
+        assertEquals("123", adapter.receiveText(), "the complete msg should be sent when last part is received")
+    }
+
+    private suspend fun WebSocketListenerChannelAdapter.receiveText(): String {
+        val frame = incomingFrames.receive()
+        assertTrue(frame is WebSocketFrame.Text)
+        return frame.text
+    }
+
+    @Test
     fun onBinaryMessage_triggersBinaryFrame() = runSuspendingTest {
         val adapter = WebSocketListenerChannelAdapter()
 
@@ -27,6 +54,38 @@ class WebSocketChannelTest {
         val frame = adapter.incomingFrames.receive()
         assertTrue(frame is WebSocketFrame.Binary)
         assertEquals(List<Byte>(2) { 42 }, frame.bytes.toList())
+    }
+
+    @Test
+    fun onBinaryMessage_partialMessages() = runSuspendingTest {
+        val adapter = WebSocketListenerChannelAdapter()
+
+        val zeroOneTwo = listOf<Byte>(0, 1, 2)
+        val oneTwo = listOf<Byte>(1, 2)
+        val threeFourFive = listOf<Byte>(3, 4, 5)
+        val one = listOf<Byte>(1)
+        val two = listOf<Byte>(2)
+        val three = listOf<Byte>(3)
+
+        launch {
+            adapter.onBinaryMessage(zeroOneTwo.toByteArray(), isLast = true)
+
+            adapter.onBinaryMessage(oneTwo.toByteArray(), isLast = false)
+            adapter.onBinaryMessage(threeFourFive.toByteArray(), isLast = true)
+
+            adapter.onBinaryMessage(one.toByteArray(), isLast = false)
+            adapter.onBinaryMessage(two.toByteArray(), isLast = false)
+            adapter.onBinaryMessage(three.toByteArray(), isLast = true)
+        }
+        assertEquals(zeroOneTwo, adapter.receiveBytes().toList())
+        assertEquals(oneTwo + threeFourFive, adapter.receiveBytes().toList())
+        assertEquals(one + two + three, adapter.receiveBytes().toList())
+    }
+
+    private suspend fun WebSocketListenerChannelAdapter.receiveBytes(): ByteArray {
+        val frame = incomingFrames.receive()
+        assertTrue(frame is WebSocketFrame.Binary)
+        return frame.bytes
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
