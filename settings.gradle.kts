fa0a57a0f82921020d945d1d6e98ce58cb3b8a82
Joffrey Bion <joffrey.bion@booking.com>
2020-04-16 19:39:58 +0200

    Add build scan config for CI builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+import com.gradle.scan.plugin.BuildScanExtension
+
+plugins {
+    id("com.gradle.enterprise") version "3.2.1"
+}
+
 rootProject.name = "krossbow"
 
 include("krossbow-stomp-core")
@@ -7,3 +13,56 @@ include("krossbow-websocket-core")
 include("krossbow-websocket-okhttp")
 include("krossbow-websocket-sockjs")
 include("krossbow-websocket-spring")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+
+        val isCIBuild = !System.getenv("CI").isNullOrEmpty()
+        publishOnFailureIf(isCIBuild)
+        tag(if (isCIBuild) "CI" else "local")
+
+        val isTravisBuild = !System.getenv("TRAVIS").isNullOrEmpty()
+        if (isTravisBuild) {
+            addTravisData()
+        }
+
+        val isGithubActionsBuild = !System.getenv("GITHUB_ACTIONS").isNullOrEmpty()
+        if (isGithubActionsBuild) {
+            addGithubActionsData()
+        }
+    }
+}
+
+fun BuildScanExtension.addTravisData() {
+    value("Build number", System.getenv("TRAVIS_BUILD_NUMBER"))
+    value("Branch", System.getenv("TRAVIS_BRANCH"))
+    value("Commit", System.getenv("TRAVIS_COMMIT"))
+    value("Commit msg", System.getenv("TRAVIS_COMMIT_MESSAGE"))
+    value("JDK", System.getenv("TRAVIS_JDK_VERSION"))
+
+    val tag = System.getenv("TRAVIS_TAG")
+    val isTagBuild = !tag.isNullOrEmpty()
+    if (isTagBuild) {
+        tag("tag")
+        value("Tag", tag)
+    }
+}
+
+fun BuildScanExtension.addGithubActionsData() {
+    value("GitHub Event", System.getenv("GITHUB_EVENT_NAME"))
+    value("GitHub Workflow", System.getenv("GITHUB_WORKFLOW"))
+    value("GitHub Run ID", System.getenv("GITHUB_RUN_ID"))
+    value("GitHub Run number", System.getenv("GITHUB_RUN_NUMBER"))
+    value("Commit", System.getenv("GITHUB_SHA"))
+
+    val ref = System.getenv("GITHUB_REF") ?: ""
+    val isTagBuild = ref.startsWith("refs/tags/")
+    if (isTagBuild) {
+        tag("tag")
+        value("Tag", ref.removePrefix("refs/tags/"))
+    } else {
+        value("Branch", ref.removePrefix("refs/heads/"))
+    }
+}
