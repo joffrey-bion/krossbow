50d8ef4d3fdc88e5dcc95a5c4dfad7508050e30f
Joffrey Bion <joffrey.bion@booking.com>
2020-05-10 02:55:59 +0200

    Add common code version of kotlinx-coroutines-test

---
+++ krossbow-stomp-core/src/commonTest/kotlin/kotlinx/coroutines/test/TestBuilders.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.coroutines.test
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.ContinuationInterceptor
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+/**
+ * Executes a [testBody] inside an immediate execution dispatcher.
+ *
+ * This is similar to [runBlocking] but it will immediately progress past delays and into [launch] and [async] blocks.
+ * You can use this to write tests that execute in the presence of calls to [delay] without causing your test to take
+ * extra time.
+ *
+ * ```
+ * @Test
+ * fun exampleTest() = runBlockingTest {
+ *     val deferred = async {
+ *         delay(1_000)
+ *         async {
+ *             delay(1_000)
+ *         }.await()
+ *     }
+ *
+ *     deferred.await() // result available immediately
+ * }
+ *
+ * ```
+ *
+ * This method requires that all coroutines launched inside [testBody] complete, or are cancelled, as part of the test
+ * conditions.
+ *
+ * Unhandled exceptions thrown by coroutines in the test will be re-thrown at the end of the test.
+ *
+ * @throws UncompletedCoroutinesError If the [testBody] does not complete (or cancel) all coroutines that it launches
+ * (including coroutines suspended on join/await).
+ *
+ * @param context additional context elements. If [context] contains [CoroutineDispatcher] or [CoroutineExceptionHandler],
+ *        then they must implement [DelayController] and [TestCoroutineExceptionHandler] respectively.
+ * @param testBody The code of the unit-test.
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+public fun runBlockingTest(
+    context: CoroutineContext = EmptyCoroutineContext,
+    testBody: suspend TestCoroutineScope.() -> Unit
+) {
+    val (safeContext, dispatcher) = context.checkArguments()
+    val startingJobs = safeContext.activeJobs()
+    val scope = TestCoroutineScope(safeContext)
+    val deferred = scope.async {
+        scope.testBody()
+    }
+    dispatcher.advanceUntilIdle()
+    deferred.getCompletionExceptionOrNull()?.let {
+        throw it
+    }
+    scope.cleanupTestCoroutines()
+    val endingJobs = safeContext.activeJobs()
+    if ((endingJobs - startingJobs).isNotEmpty()) {
+        throw UncompletedCoroutinesError("Test finished with active jobs: $endingJobs")
+    }
+}
+
+private fun CoroutineContext.activeJobs(): Set<Job> {
+    return checkNotNull(this[Job]).children.filter { it.isActive }.toSet()
+}
+
+/**
+ * Convenience method for calling [runBlockingTest] on an existing [TestCoroutineScope].
+ */
+public fun TestCoroutineScope.runBlockingTest(block: suspend TestCoroutineScope.() -> Unit): Unit =
+    runBlockingTest(coroutineContext, block)
+
+/**
+ * Convenience method for calling [runBlockingTest] on an existing [TestCoroutineDispatcher].
+ */
+public fun TestCoroutineDispatcher.runBlockingTest(block: suspend TestCoroutineScope.() -> Unit): Unit =
+    runBlockingTest(this, block)
+
+private fun CoroutineContext.checkArguments(): Pair<CoroutineContext, DelayController> {
+    // TODO optimize it
+    val dispatcher = get(ContinuationInterceptor).run {
+        this?.let { require(this is DelayController) { "Dispatcher must implement DelayController: $this" } }
+        this ?: TestCoroutineDispatcher()
+    }
+
+    val exceptionHandler = get(CoroutineExceptionHandler).run {
+        this?.let {
+            require(this is UncaughtExceptionCaptor) { "coroutineExceptionHandler must implement UncaughtExceptionCaptor: $this" }
+        }
+        this ?: TestCoroutineExceptionHandler()
+    }
+
+    val job = get(Job) ?: SupervisorJob()
+    return Pair(this + dispatcher + exceptionHandler + job, dispatcher as DelayController)
+}
---
+++ krossbow-stomp-core/src/commonTest/kotlin/kotlinx/coroutines/test/TestCoroutineExceptionHandler.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.coroutines.test
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Access uncaught coroutine exceptions captured during test execution.
+ */
+public interface UncaughtExceptionCaptor {
+    /**
+     * List of uncaught coroutine exceptions.
+     *
+     * The returned list is a copy of the currently caught exceptions.
+     * During [cleanupTestCoroutines] the first element of this list is rethrown if it is not empty.
+     */
+    public val uncaughtExceptions: List<Throwable>
+
+    /**
+     * Call after the test completes to ensure that there were no uncaught exceptions.
+     *
+     * The first exception in uncaughtExceptions is rethrown. All other exceptions are
+     * printed using [Throwable.printStackTrace].
+     *
+     * @throws Throwable the first uncaught exception, if there are any uncaught exceptions.
+     */
+    public fun cleanupTestCoroutines()
+}
+
+/**
+ * An exception handler that captures uncaught exceptions in tests.
+ */
+public class TestCoroutineExceptionHandler : AbstractCoroutineContextElement(CoroutineExceptionHandler),
+    UncaughtExceptionCaptor, CoroutineExceptionHandler {
+    private val _exceptions = mutableListOf<Throwable>()
+
+    /** @suppress **/
+    override fun handleException(context: CoroutineContext, exception: Throwable) {
+        synchronized(_exceptions) {
+            _exceptions += exception
+        }
+    }
+
+    /** @suppress **/
+    override val uncaughtExceptions: List<Throwable>
+        get() = synchronized(_exceptions) { _exceptions.toList() }
+
+    /** @suppress **/
+    override fun cleanupTestCoroutines() {
+        synchronized(_exceptions) {
+            val exception = _exceptions.firstOrNull() ?: return
+            // log the rest
+            _exceptions.drop(1).forEach { "${it::class.simpleName}: ${it.message}" }
+            throw exception
+        }
+    }
+}
