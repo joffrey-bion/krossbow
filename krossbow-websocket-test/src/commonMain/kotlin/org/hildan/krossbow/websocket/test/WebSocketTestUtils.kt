5b4264d7f8d53bffd2db691231244d9908c24cfb
Joffrey Bion <joffrey.bion@booking.com>
2020-09-27 17:17:33 +0200

    Fix style

--- krossbow-websocket-test/src/commonMain/kotlin/org/hildan/krossbow/websocket/test/WebSocketTestUtils.kt
+++ krossbow-websocket-test/src/commonMain/kotlin/org/hildan/krossbow/websocket/test/WebSocketTestUtils.kt
@@ -5,4 +5,3 @@ import kotlinx.coroutines.CoroutineScope
 expect fun runSuspendingTest(block: suspend CoroutineScope.() -> Unit)
 
 expect suspend fun runAlongEchoWSServer(block: suspend (port: Int) -> Unit)
-
