0d49964c4c4909055e1e8f92fee7fe6ee630395c
joffrey-bion <joffrey.bion@gmail.com>
2020-11-17 01:52:16 +0100

    Support backpressure in OkHttp implementation

--- krossbow-websocket-okhttp/src/main/kotlin/org/hildan/krossbow/websocket/okhttp/OkHttpWebSocketClient.kt
+++ krossbow-websocket-okhttp/src/main/kotlin/org/hildan/krossbow/websocket/okhttp/OkHttpWebSocketClient.kt
@@ -1,12 +1,7 @@
 package org.hildan.krossbow.websocket.okhttp
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -16,7 +11,6 @@ import okio.ByteString
 import okio.ByteString.Companion.toByteString
 import org.hildan.krossbow.websocket.WebSocketFrame
 import org.hildan.krossbow.websocket.WebSocketListenerChannelAdapter
-import kotlin.coroutines.CoroutineContext
 import org.hildan.krossbow.websocket.WebSocketClient as KrossbowWebSocketClient
 import org.hildan.krossbow.websocket.WebSocketSession as KrossbowWebSocketSession
 
@@ -26,47 +20,37 @@ class OkHttpWebSocketClient(
 
     override suspend fun connect(url: String): KrossbowWebSocketSession {
         val request = Request.Builder().url(url).build()
-        val listener = KrossbowToOkHttpListenerAdapter()
-        val okWebsocket = withContext(Dispatchers.IO) { client.newWebSocket(request, listener) }
-        return OkHttpSocketToKrossbowSessionAdapter(okWebsocket, listener)
+        val channelListener = WebSocketListenerChannelAdapter()
+        val okHttpListener = KrossbowToOkHttpListenerAdapter(channelListener)
+        val okWebsocket = withContext(Dispatchers.IO) { client.newWebSocket(request, okHttpListener) }
+        return OkHttpSocketToKrossbowSessionAdapter(okWebsocket, channelListener.incomingFrames)
     }
 }
 
-private class KrossbowToOkHttpListenerAdapter : WebSocketListener(), CoroutineScope {
-
-    private val job = Job()
-
-    override val coroutineContext: CoroutineContext
-        get() = job
-
-    val channelListener: WebSocketListenerChannelAdapter = WebSocketListenerChannelAdapter()
+private class KrossbowToOkHttpListenerAdapter(
+    private val channelListener: WebSocketListenerChannelAdapter
+) : WebSocketListener() {
 
     override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
-        launch { channelListener.onBinaryMessage(bytes.toByteArray()) }
+        runBlocking { channelListener.onBinaryMessage(bytes.toByteArray()) }
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
-        launch { channelListener.onTextMessage(text) }
+        runBlocking { channelListener.onTextMessage(text) }
     }
 
     override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-        launch { channelListener.onClose(code, reason) }
-        job.cancel()
+        runBlocking { channelListener.onClose(code, reason) }
     }
 
     override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
         channelListener.onError(t)
-        job.cancel()
-    }
-
-    suspend fun stop() {
-        job.cancelAndJoin()
     }
 }
 
 private class OkHttpSocketToKrossbowSessionAdapter(
     private val okSocket: WebSocket,
-    private val listener: KrossbowToOkHttpListenerAdapter
+    framesChannel: ReceiveChannel<WebSocketFrame>
 ) : KrossbowWebSocketSession {
 
     override val url: String
@@ -75,7 +59,7 @@ private class OkHttpSocketToKrossbowSessionAdapter(
     override val canSend: Boolean
         get() = true // all send methods are just no-ops when the session is closed, so always OK
 
-    override val incomingFrames: ReceiveChannel<WebSocketFrame> = listener.channelListener.incomingFrames
+    override val incomingFrames: ReceiveChannel<WebSocketFrame> = framesChannel
 
     override suspend fun sendText(frameText: String) {
         okSocket.send(frameText)
@@ -87,6 +71,5 @@ private class OkHttpSocketToKrossbowSessionAdapter(
 
     override suspend fun close(code: Int, reason: String?) {
         okSocket.close(code, reason)
-        listener.stop()
     }
 }
