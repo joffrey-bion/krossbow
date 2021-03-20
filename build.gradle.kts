fd4782f452d80f2a63b259cbf6fc6e17427a220e
joffrey-bion <joffrey.bion@gmail.com>
2021-03-20 19:32:54 +0100

    Publish to Maven Central
    Resolves:
    https://github.com/joffrey-bion/krossbow/issues/93

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-import com.jfrog.bintray.gradle.BintrayExtension
-import com.jfrog.bintray.gradle.BintrayExtension.*
-import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
-import org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact
 import org.hildan.github.changelog.builder.DEFAULT_EXCLUDED_LABELS
 
 plugins {
@@ -12,9 +8,11 @@ plugins {
     kotlin("plugin.spring") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
     id("org.jetbrains.dokka") version "1.4.30" apply false
-    id("com.jfrog.bintray") version "1.8.5" apply false
     id("org.hildan.github.changelog") version "1.6.0"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.2.3"
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
 
 allprojects {
@@ -26,13 +24,8 @@ allprojects {
 
     apply(plugin = "org.jetbrains.dokka")
 
-    val dokkaJar by tasks.creating(Jar::class) {
-        archiveClassifier.set("javadoc")
-        from(tasks.findByName("dokkaHtml"))
-    }
-
     afterEvaluate {
-        // suppressing Dokka generation for JS because of the annoying ZipException on NPM dependencies
+        // suppressing Dokka generation for JS because of the ZipException on NPM dependencies
         // https://github.com/Kotlin/dokka/issues/537
         tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
             dokkaSourceSets.findByName("jsMain")?.suppress?.set(true)
@@ -41,11 +34,9 @@ allprojects {
     }
 }
 
-val Project.githubUser get() = getPropOrEnv("githubUser", "GITHUB_USER")
+val Project.githubUser: String? get() = findProperty("githubUser") as String? ?: System.getenv("GITHUB_USER")
 val githubSlug = "$githubUser/${rootProject.name}"
 val githubRepoUrl = "https://github.com/$githubSlug"
-val Project.labels get() = arrayOf("websocket", "stomp", "krossbow", "multiplatform", "kotlin", "client")
-val Project.licenses get() = arrayOf("MIT")
 
 changelog {
     githubUser = project.githubUser
@@ -54,9 +45,16 @@ changelog {
     customTagByIssueNumber = mapOf(6 to "0.1.1", 10 to "0.1.2", 15 to "0.4.0")
 }
 
+nexusPublishing {
+    packageGroup.set("org.hildan")
+    repositories {
+        sonatype()
+    }
+}
+
 subprojects {
     apply(plugin = "maven-publish")
-    apply(plugin = "com.jfrog.bintray")
+    apply(plugin = "signing")
 
     val compilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -64,7 +62,6 @@ subprojects {
     }
     tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>> {
         kotlinOptions.freeCompilerArgs += compilerArgs
-        //kotlinOptions.allWarningsAsErrors = true
     }
 
     tasks.withType<AbstractTestTask> {
@@ -75,70 +72,28 @@ subprojects {
         }
     }
 
+    val dokkaJar by tasks.creating(Jar::class) {
+        archiveClassifier.set("javadoc")
+        from(tasks.findByName("dokkaHtml"))
+    }
+
     afterEvaluate {
         val publications = extensions.getByType<PublishingExtension>().publications
         publications.filterIsInstance<MavenPublication>().forEach { pub ->
-            pub.artifact(tasks.findByName("dokkaJar"))
+            pub.artifact(dokkaJar)
             pub.configurePomForMavenCentral(project)
         }
 
-        extensions.configure<BintrayExtension>("bintray") {
-            user = getPropOrEnv("bintrayUser", "BINTRAY_USER")
-            key = getPropOrEnv("bintrayApiKey", "BINTRAY_KEY")
-            setPublications(*publications.names.toTypedArray())
-            publish = true
-
-            pkg(closureOf<PackageConfig> {
-                repo = getPropOrEnv("bintrayRepo", "BINTRAY_REPO")
-                name = project.name
-                desc = project.description
-                setLabels(*project.labels)
-                setLicenses(*project.licenses)
-
-                websiteUrl = githubRepoUrl
-                issueTrackerUrl = "$githubRepoUrl/issues"
-                vcsUrl = "$githubRepoUrl.git"
-                githubRepo = githubSlug
-
-                version(closureOf<VersionConfig> {
-                    desc = project.description
-                    vcsTag = project.version.toString()
-                    gpg(closureOf<GpgConfig> {
-                        sign = true
-                    })
-                    mavenCentralSync(closureOf<MavenCentralSyncConfig> {
-                        sync = false // TODO re-activate when javadoc stuff is fixed
-                        user = getPropOrEnv("ossrhUserToken", "OSSRH_USER_TOKEN")
-                        password = getPropOrEnv("ossrhKey", "OSSRH_KEY")
-                    })
-                })
-            })
+        signing {
+            val signingKey: String? by project
+            val signingPassword: String? by project
+            useInMemoryPgpKeys(signingKey, signingPassword)
+            sign(publishing.publications)
         }
 
         tasks["assemble"].dependsOn(tasks["dokkaHtml"])
-        tasks["bintrayUpload"].dependsOn(tasks["build"])
-
-        // Workaround bintray plugin issue for Gradle metadata publishing
-        // https://github.com/bintray/gradle-bintray-plugin/issues/229
-        tasks.withType<BintrayUploadTask> {
-            doFirst {
-                publications
-                    .filterIsInstance<MavenPublication>()
-                    .forEach { pub ->
-                        val moduleFile = buildDir.resolve("publications/${pub.name}/module.json")
-                        if (moduleFile.exists()) {
-                            pub.artifact(object : FileBasedMavenArtifact(moduleFile) {
-                                override fun getDefaultExtension() = "module"
-                            })
-                        }
-                    }
-            }
-        }
     }
 }
-
-fun Project.getPropOrEnv(propName: String, envVar: String? = null): String? =
-  findProperty(propName) as String? ?: System.getenv(envVar)
 
 fun MavenPublication.configurePomForMavenCentral(project: Project) = pom {
     name.set(project.name)
