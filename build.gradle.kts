7ff0ed36f1be661df5b72d49c3560cd44ba9034f
joffrey-bion <joffrey.bion@gmail.com>
2021-07-27 17:57:30 +0200

    Upgrade Dokka to 1.5.0
    Resolves:
    https://github.com/joffrey-bion/krossbow/issues/127

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("multiplatform") version kotlinVersion apply false
     kotlin("plugin.spring") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
-    id("org.jetbrains.dokka") version "1.4.32" apply false
+    id("org.jetbrains.dokka") version "1.5.0" apply false
     id("org.hildan.github.changelog") version "1.6.0"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.6.0"
     `maven-publish`
