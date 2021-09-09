072e9d3959852bbc30e8ebce525821b184d4b7c0
joffrey-bion <joffrey.bion@gmail.com>
2021-09-09 03:01:57 +0200

    Enable JS autobahn tests

--- krossbow-websocket-core/src/jsTest/kotlin/org/hildan/krossbow/websocket/js/JsWebSocketAutobahnTest.kt
+++ krossbow-websocket-core/src/jsTest/kotlin/org/hildan/krossbow/websocket/js/JsWebSocketAutobahnTest.kt
@@ -4,9 +4,7 @@ import IsomorphicWebSocket
 import org.hildan.krossbow.websocket.WebSocketClient
 import org.hildan.krossbow.websocket.test.autobahn.AutobahnClientTestSuite
 import org.hildan.krossbow.websocket.test.isBrowser
-import kotlin.test.Ignore
 
-@Ignore // JS tests currently cannot run on macOS runners because we can't read Autobahn's environment info
 class JsWebSocketAutobahnTest : AutobahnClientTestSuite("krossbow-js-client-${environment()}") {
 
     override fun provideClient(): WebSocketClient = JsWebSocketClientAdapter { url -> IsomorphicWebSocket(url) }
