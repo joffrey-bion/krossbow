b8ae12d1effa5fb13840e0b023ff834774a4402a
joffrey-bion <joffrey.bion@gmail.com>
2021-10-10 19:11:38 +0200

    Add FIXME comment on ignored tests

--- autobahn-tests/src/jvmTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/KtorJavaClientAutobahnTests.kt
+++ autobahn-tests/src/jvmTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/KtorJavaClientAutobahnTests.kt
@@ -6,7 +6,7 @@ import io.ktor.client.features.websocket.*
 import org.hildan.krossbow.websocket.ktor.KtorWebSocketClient
 import kotlin.test.Ignore
 
-@Ignore // ignored because double ping-pong management fails Autobahn tests 2.x
+@Ignore // FIXME ignored because double ping-pong management fails Autobahn tests 2.x
 class KtorJavaClientAutobahnTests : AutobahnClientTestSuite("krossbow-ktor-java-client") {
 
     override fun provideClient() = KtorWebSocketClient(HttpClient(Java) { install(WebSockets) })
