aed0f831b21cbefaae01b79a4cc8f31a9888865b
joffrey-bion <joffrey.bion@gmail.com>
2021-10-13 02:46:55 +0200

    Fix @Ignore reason for Ktor JS autobahn tests

--- autobahn-tests/src/jsTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/KtorJsClientAutobahnTests.kt
+++ autobahn-tests/src/jsTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/KtorJsClientAutobahnTests.kt
@@ -4,7 +4,11 @@ import org.hildan.krossbow.websocket.ktor.KtorWebSocketClient
 import org.hildan.krossbow.websocket.test.environment
 import kotlin.test.Ignore
 
-@Ignore // like other JS autobahn tests, cannot run at the moment on macOS runner
+// FIXME part of these tests fail due to getCaseStatus abrupt connection close
+// FIXME part of these tests fail with IllegalStateException: Already resumed, but proposed with update CompletedExceptionally
+//   The underlying error is part of the test, for instance: "nvalid WebSocket frame: RSV1 must be clear"
+//   But for some reason Ktor tries to resume a continuation twice?
+@Ignore
 class KtorJsClientAutobahnTests : AutobahnClientTestSuite("krossbow-ktor-js-client-${environment()}") {
 
     override fun provideClient() = KtorWebSocketClient()
