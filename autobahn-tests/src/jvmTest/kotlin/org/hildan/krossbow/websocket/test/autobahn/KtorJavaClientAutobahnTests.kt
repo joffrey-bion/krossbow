376732e9739e7ee5a9b118ec63d4da4ddef60e61
joffrey-bion <joffrey.bion@gmail.com>
2021-10-16 11:24:08 +0200

    Unignore Ktor Java client's tests, and instead exclude tests that actually timeout

--- autobahn-tests/src/jvmTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/KtorJavaClientAutobahnTests.kt
+++ autobahn-tests/src/jvmTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/KtorJavaClientAutobahnTests.kt
@@ -4,15 +4,20 @@ import io.ktor.client.*
 import io.ktor.client.engine.java.*
 import io.ktor.client.features.websocket.*
 import org.hildan.krossbow.websocket.ktor.KtorWebSocketClient
-import kotlin.test.Ignore
 
-@Ignore // FIXME weird timeouts on lots of cases
 class KtorJavaClientAutobahnTests : AutobahnClientTestSuite(
     agentUnderTest = "krossbow-ktor-java-client",
     exclusions = listOf(
         CaseExclusion(
             caseIdPrefixes = listOf("2.", "5.6", "5.7", "5.8"),
             reason = "Ktor sends double pongs with Java engine, which breaks autobahn ping-pong tests",
+        ),
+        // FIXME this exclusions should be investigated and fixed
+        CaseExclusion(
+            caseIdPrefixes = listOf(
+                "3.1", "3.5", "3.6", "3.7", "4.1.1", "4.1.2", "4.2.1", "4.2.2", "5.1", "5.2", "5.9",
+            ),
+            reason = "This test times out every time (to be investigated)",
         ),
     ),
 ) {
