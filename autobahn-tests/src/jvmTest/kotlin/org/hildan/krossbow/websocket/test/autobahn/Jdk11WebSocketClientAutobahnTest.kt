cd01771810d6f0a405f1a7a44f2377ad6dc5a6fc
joffrey-bion <joffrey.bion@gmail.com>
2021-11-19 01:14:40 +0100

    Ignore autobahn ping-pong test cases (2.10, 2.11) for JDK11
    These test cases are invalid. JDK11 sometimes only sends the PONG
    corresponding to the last PING of a series, and this conforms to
    the spec, but is incorrectly marked as failed by Autobahn.
    
    See https://github.com/crossbario/autobahn-testsuite/issues/101

--- autobahn-tests/src/jvmTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/Jdk11WebSocketClientAutobahnTest.kt
+++ autobahn-tests/src/jvmTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/Jdk11WebSocketClientAutobahnTest.kt
@@ -2,7 +2,15 @@ package org.hildan.krossbow.websocket.test.autobahn
 
 import org.hildan.krossbow.websocket.jdk.Jdk11WebSocketClient
 
-class Jdk11WebSocketClientAutobahnTest : AutobahnClientTestSuite("krossbow-jdk11-client") {
-
+class Jdk11WebSocketClientAutobahnTest : AutobahnClientTestSuite(
+    agentUnderTest = "krossbow-jdk11-client",
+    exclusions = listOf(
+        CaseExclusion(
+            caseIdPrefixes = listOf("2.10", "2.11"),
+            reason = "Autobahn ping-pong tests are stricter than the spec. The JDK11 client sometimes just sends the " +
+                "last PONG, which is conform to the spec and should be accepted by the test.",
+        ),
+    ),
+) {
     override fun provideClient() = Jdk11WebSocketClient()
 }
