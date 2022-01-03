be87da81d505e4bc0aef4b83baa21bcd47e84193
joffrey-bion <joffrey.bion@gmail.com>
2022-01-03 19:08:27 +0100

    Add OS name in agent-under-test

--- autobahn-tests/src/nativeDarwinTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/DarwinWebSocketAutobahnTest.kt
+++ autobahn-tests/src/nativeDarwinTest/kotlin/org/hildan/krossbow/websocket/test/autobahn/DarwinWebSocketAutobahnTest.kt
@@ -3,7 +3,7 @@ package org.hildan.krossbow.websocket.test.autobahn
 import org.hildan.krossbow.websocket.WebSocketClient
 import org.hildan.krossbow.websocket.darwin.DarwinWebSocketClient
 
-class DarwinWebSocketAutobahnTest : AutobahnClientTestSuite("krossbow-darwin-client") {
+class DarwinWebSocketAutobahnTest : AutobahnClientTestSuite("krossbow-darwin-client-${Platform.osFamily.name.lowercase()}") {
 
     override fun provideClient(): WebSocketClient = DarwinWebSocketClient()
 }
