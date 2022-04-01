48237f0b943b8f15613d85b72a02f870f31ece81
Remy Bardou <remy.bardou@epicgames.com>
2022-04-01 19:01:09 +0200

    Fix iOS Threading issue

--- krossbow-stomp-core/src/commonMain/kotlin/org/hildan/krossbow/stomp/heartbeats/HeartBeats.kt
+++ krossbow-stomp-core/src/commonMain/kotlin/org/hildan/krossbow/stomp/heartbeats/HeartBeats.kt
@@ -1,8 +1,10 @@
 package org.hildan.krossbow.stomp.heartbeats
 
 import org.hildan.krossbow.stomp.config.HeartBeat
+import kotlin.native.concurrent.SharedImmutable
 import kotlin.time.Duration
 
+@SharedImmutable
 internal val NO_HEART_BEATS = HeartBeat(Duration.ZERO, Duration.ZERO)
 
 internal fun HeartBeat.negotiated(serverHeartBeats: HeartBeat?): HeartBeat = HeartBeat(
