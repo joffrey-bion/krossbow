75c96b85acaa14d591ced7a32c77320ee0f1f560
joffrey-bion <joffrey.bion@gmail.com>
2022-04-20 23:26:50 +0200

    Upgrade Kotlin to 1.6.21
    Resolves:
    https://github.com/joffrey-bion/krossbow/issues/184

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.6.21"
     kotlin("jvm") apply false
     kotlin("js") apply false
     kotlin("multiplatform") apply false
     kotlin("plugin.spring") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
-    id("org.jetbrains.dokka") version "1.6.10" apply false
+    id("org.jetbrains.dokka") version "1.6.20" apply false
     id("org.hildan.github.changelog") version "1.11.1"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.8.0"
     `maven-publish`
