d647d01a5420a4d079734470dfe4a2d82e38892c
joffrey-bion <joffrey.bion@gmail.com>
2022-06-03 20:03:38 +0200

    Upgrade binary compatibility validator to 0.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("plugin.serialization") version kotlinVersion apply false
     id("org.jetbrains.dokka") version kotlinVersion apply false
     id("org.hildan.github.changelog") version "1.11.1"
-    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.8.0"
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.10.0"
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
