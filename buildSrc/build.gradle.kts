46497856352d6cfaf3472e709e8df26bb1fbdcfb
joffrey-bion <joffrey.bion@gmail.com>
2022-06-21 17:30:59 +0200

    Upgrade Dokka to 1.7.0
    Resolves:
    https://github.com/joffrey-bion/krossbow/issues/244

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,10 @@ dependencies {
     implementation(gradleKotlinDsl())
 
     implementation(kotlin("gradle-plugin", "1.7.0"))
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.21")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.0") {
+        // workaround while waiting for https://github.com/Kotlin/dokka/pull/2543
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+    }
 }
 
 gradlePlugin {
