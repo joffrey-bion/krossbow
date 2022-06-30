3612eb3fd5ab1f025c29d75ea2c7e3b07a3b33f2
joffrey-bion <joffrey.bion@gmail.com>
2022-06-30 17:53:23 +0200

    Simplify safeIs()

--- krossbow-websocket-ktor-legacy/src/jvmMain/kotlin/org/hildan/krossbow/websocket/ktor/HandshakeExceptionsJvm.kt
+++ krossbow-websocket-ktor-legacy/src/jvmMain/kotlin/org/hildan/krossbow/websocket/ktor/HandshakeExceptionsJvm.kt
@@ -6,18 +6,13 @@ import java.net.http.WebSocketHandshakeException
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
-internal actual fun extractHandshakeStatusCode(handshakeException: Exception): Int? {
-    val e = handshakeException
-    return when {
-        // no status code if we can't even contact the host
-        e is UnknownHostException -> null
-        // with OkHttp engine, we get ProtocolException with itself as cause - we can only parse the message
-        e is ProtocolException -> extractHandshakeStatusCode(e)
-        e.safeIs<WebSocketHandshakeException>("java.net.http.WebSocketHandshakeException") -> {
-            extractHandshakeStatusCode(e)
-        }
-        else -> null
-    }
+internal actual fun extractHandshakeStatusCode(handshakeException: Exception): Int? = when {
+    // no status code if we can't even contact the host
+    handshakeException is UnknownHostException -> null
+    // with OkHttp engine, we get ProtocolException with itself as cause - we can only parse the message
+    handshakeException is ProtocolException -> extractHandshakeStatusCode(handshakeException)
+    handshakeException.safeIs<WebSocketHandshakeException>() -> extractHandshakeStatusCode(handshakeException)
+    else -> null
 }
 
 private val protocolExceptionMessageRegex = Regex("""Expected HTTP 101 response but was '(\d{3}) [^']+'""")
@@ -32,32 +27,17 @@ private fun extractHandshakeStatusCode(webSocketHandshakeException: WebSocketHan
 
 /**
  * Returns true if [C] is on the classpath and `this` is an instance of [C].
- * The given [className] must match the fully qualified name of [C].
  *
  * Doesn't fail with [NoClassDefFoundError] if [C] is not present.
  */
 @OptIn(ExperimentalContracts::class)
-private inline fun <reified C : Any> Any.safeIs(className: String): Boolean {
+private inline fun <reified C : Any> Any.safeIs(): Boolean {
     contract {
         returns(true) implies(this@safeIs is C)
     }
-    if (!classExists(className)) {
-        return false
+    return try {
+        this is C
+    } catch (e: NoClassDefFoundError) {
+        false
     }
-    checkMatches<C>(className) // prevent developer mistakes
-    return this is C
-}
-
-private inline fun <reified T : Any> checkMatches(className: String) {
-    val typeName = T::class.qualifiedName
-    require(typeName == className) {
-        "Mismatch between the given class name '$className' and the actual parameter type of the action lambda '$typeName"
-    }
-}
-
-private fun classExists(className: String): Boolean = try {
-    Class.forName(className)
-    true
-} catch (e: ClassNotFoundException) {
-    false
 }
