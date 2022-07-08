1925bce3c74b7286958f1c7a27c94b148f56edca
Joffrey Bion <joffrey.bion@gmail.com>
2022-07-09 00:32:13 +0200

    Upgrade Kotlin to 1.7.10
    Resolves:
    https://github.com/joffrey-bion/krossbow/issues/253

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation(gradleApi())
     implementation(gradleKotlinDsl())
 
-    implementation(kotlin("gradle-plugin", "1.7.0"))
+    implementation(kotlin("gradle-plugin", "1.7.10"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.0") {
         // workaround while waiting for https://github.com/Kotlin/dokka/pull/2543
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
