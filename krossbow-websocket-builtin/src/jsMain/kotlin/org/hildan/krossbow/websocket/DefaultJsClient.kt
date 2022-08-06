3be0fb3043c7e70232f772344cc7d07171ba3247
Joffrey Bion <joffrey.bion@gmail.com>
2022-08-06 19:29:44 +0200

    Ensure deprecation annotations are shown on all platforms for `WebSocketClient.default()`
    This duplication of the @Deprecated annotation is necessary to show it when using directly the actual declarations (e.g. in a pure JVM project).

--- krossbow-websocket-builtin/src/jsMain/kotlin/org/hildan/krossbow/websocket/DefaultJsClient.kt
+++ krossbow-websocket-builtin/src/jsMain/kotlin/org/hildan/krossbow/websocket/DefaultJsClient.kt
@@ -2,4 +2,8 @@ package org.hildan.krossbow.websocket
 
 import org.hildan.krossbow.websocket.builtin.builtIn
 
+@Deprecated(
+    message = "Renamed to builtIn(), and moved to the package org.hildan.krossbow.websocket.builtin.builtIn",
+    ReplaceWith("builtIn()", imports = ["org.hildan.krossbow.websocket.builtin.builtIn"]),
+)
 actual fun WebSocketClient.Companion.default(): WebSocketClient = builtIn()
