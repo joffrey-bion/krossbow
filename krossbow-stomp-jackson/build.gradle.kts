332ed2f2ba0fdc42abf8ce713e19f709b53635ea
Joffrey Bion <joffrey.bion@gmail.com>
2022-10-24 22:27:44 +0200

    Use jackson bom to fix CVE-2022-42003
    Resolves:
    https://github.com/joffrey-bion/krossbow/issues/277

--- krossbow-stomp-jackson/build.gradle.kts
+++ krossbow-stomp-jackson/build.gradle.kts
@@ -7,11 +7,12 @@ description = "An extension of Krossbow STOMP client using Jackson for message c
 
 dependencies {
     api(projects.krossbowStompCore)
+    api(platform(libs.jackson.bom))
     api(libs.jackson.core)
     api(libs.jackson.module.kotlin)
 
     testImplementation(kotlin("test"))
 }
 
-val jacksonMinorVersion = libs.versions.jackson.get().substringBeforeLast(".")
+val jacksonMinorVersion = libs.versions.jackson.get().split(".").take(2).joinToString(".")
 dokkaExternalDocLink("https://fasterxml.github.io/jackson-databind/javadoc/$jacksonMinorVersion/")
