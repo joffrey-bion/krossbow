4128007eeee8606a8cacf17385fe25d0626a6dbb
Joffrey Bion <joffrey.bion@gmail.com>
2022-10-24 22:13:15 +0200

    Upgrade github info plugin to 1.4.0 to fix links in the pom

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 
     implementation(kotlin("gradle-plugin", "1.7.20"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
-    implementation("ru.vyarus:gradle-github-info-plugin:1.3.0")
+    implementation("ru.vyarus:gradle-github-info-plugin:1.4.0")
 }
 
 gradlePlugin {
