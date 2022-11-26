d3a8c2b67de66f93f5efc0e122acc64b4a3e00b5
Joffrey Bion <joffrey.bion@gmail.com>
2022-11-26 15:02:34 +0100

    Update github changelog plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("plugin.spring") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
     id("org.jetbrains.dokka")
-    id("org.hildan.github.changelog") version "1.11.1"
+    id("org.hildan.github.changelog") version "1.12.1"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.10.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
     id("com.louiscad.complete-kotlin") version "1.1.0" // for autocomplete of Apple libraries on non-macOS systems
