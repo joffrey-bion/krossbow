b590afc25fa382d1aee2f0aa3ea03c623bfe679c
Joffrey Bion <joffrey.bion@gmail.com>
2023-10-14 12:36:04 +0200

    Enable build scan all the time, and remove unnecessary GitHub data

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
-import com.gradle.scan.plugin.BuildScanExtension
 import de.fayard.refreshVersions.core.DependencySelection
 import de.fayard.refreshVersions.core.StabilityLevel
 
@@ -30,32 +29,7 @@ gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
-
-        val isCIBuild = !System.getenv("CI").isNullOrEmpty()
-        publishAlwaysIf(isCIBuild)
-        tag(if (isCIBuild) "CI" else "local")
-
-        val isGithubActionsBuild = !System.getenv("GITHUB_ACTIONS").isNullOrEmpty()
-        if (isGithubActionsBuild) {
-            addGithubActionsData()
-        }
-    }
-}
-
-fun BuildScanExtension.addGithubActionsData() {
-    value("GitHub Event", System.getenv("GITHUB_EVENT_NAME"))
-    value("GitHub Workflow", System.getenv("GITHUB_WORKFLOW"))
-    value("GitHub Run ID", System.getenv("GITHUB_RUN_ID"))
-    value("GitHub Run number", System.getenv("GITHUB_RUN_NUMBER"))
-    value("Commit", System.getenv("GITHUB_SHA"))
-
-    val ref = System.getenv("GITHUB_REF") ?: ""
-    val isTagBuild = ref.startsWith("refs/tags/")
-    if (isTagBuild) {
-        tag("tag")
-        value("Tag", ref.removePrefix("refs/tags/"))
-    } else {
-        value("Branch", ref.removePrefix("refs/heads/"))
+        publishAlways()
     }
 }
 
