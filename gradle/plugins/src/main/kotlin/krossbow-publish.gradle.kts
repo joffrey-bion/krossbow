83411d275eb1a525b37496b0a9ac7caa5f702f92
Joffrey Bion <joffrey.bion@gmail.com>
2024-02-01 01:00:56 +0100

    Upgrade kotlin-publish plugin to 1.4.0

--- gradle/plugins/src/main/kotlin/krossbow-publish.gradle.kts
+++ gradle/plugins/src/main/kotlin/krossbow-publish.gradle.kts
@@ -29,22 +29,3 @@ signing {
     useInMemoryPgpKeys(signingKey, signingPassword)
     sign(extensions.getByType<PublishingExtension>().publications)
 }
-
-// Resolves issues with .asc task output of the sign task of native targets.
-// See: https://github.com/gradle/gradle/issues/26132
-// And: https://youtrack.jetbrains.com/issue/KT-46466
-// And: https://youtrack.jetbrains.com/issue/KT-61313
-tasks.withType<Sign>().configureEach {
-    val pubName = name.removePrefix("sign").removeSuffix("Publication")
-
-    // These tasks only exist for native targets, hence findByName() to avoid trying to find them for other targets
-
-    // Task ':linkDebug<platform>' uses this output of task ':sign<platform>Publication' without declaring an explicit or implicit dependency
-    tasks.findByName("linkDebugTest$pubName")?.let {
-        mustRunAfter(it)
-    }
-    // Task ':compileTestKotlin<platform>' uses this output of task ':sign<platform>Publication' without declaring an explicit or implicit dependency
-    tasks.findByName("compileTestKotlin$pubName")?.let {
-        mustRunAfter(it)
-    }
-}
