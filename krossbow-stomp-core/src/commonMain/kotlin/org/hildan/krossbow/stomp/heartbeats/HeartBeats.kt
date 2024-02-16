dfbd7c00307451e3b9ab550f4777168d6da1fed0
Joffrey Bion <joffrey.bion@gmail.com>
2024-02-16 03:51:41 +0100

    Remove unnecessary usage of deprecated @SharedImmutable

--- krossbow-stomp-core/src/commonMain/kotlin/org/hildan/krossbow/stomp/heartbeats/HeartBeats.kt
+++ krossbow-stomp-core/src/commonMain/kotlin/org/hildan/krossbow/stomp/heartbeats/HeartBeats.kt
@@ -1,10 +1,8 @@
 package org.hildan.krossbow.stomp.heartbeats
 
 import org.hildan.krossbow.stomp.config.HeartBeat
-import kotlin.native.concurrent.SharedImmutable
 import kotlin.time.Duration
 
-@SharedImmutable
 internal val NO_HEART_BEATS = HeartBeat(Duration.ZERO, Duration.ZERO)
 
 internal fun HeartBeat.negotiated(serverHeartBeats: HeartBeat?): HeartBeat = HeartBeat(
