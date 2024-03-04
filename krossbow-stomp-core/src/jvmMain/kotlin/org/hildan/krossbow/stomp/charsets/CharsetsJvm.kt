f7b877bd2efdeda97bed23f1644b789379e05fdf
Joffrey Bion <joffrey.bion@gmail.com>
2024-03-04 19:42:08 +0100

    Use non-copying ByteString conversions in CharsetsJvm.kt

--- krossbow-stomp-core/src/jvmMain/kotlin/org/hildan/krossbow/stomp/charsets/CharsetsJvm.kt
+++ krossbow-stomp-core/src/jvmMain/kotlin/org/hildan/krossbow/stomp/charsets/CharsetsJvm.kt
@@ -1,19 +1,20 @@
 package org.hildan.krossbow.stomp.charsets
 
-import kotlinx.io.*
 import kotlinx.io.bytestring.*
-import java.nio.ByteBuffer
+import kotlinx.io.bytestring.unsafe.*
+import org.hildan.krossbow.io.*
 import java.nio.charset.Charset
 
 actual typealias Charset = Charset
 
 actual typealias Charsets = kotlin.text.Charsets
 
+@OptIn(UnsafeByteStringApi::class)
 @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
 internal actual fun String.encodeToByteString(charset: Charset): ByteString =
-    ByteString((this as java.lang.String).getBytes(charset))
+    (this as java.lang.String).getBytes(charset).asByteString()
 
-internal actual fun ByteString.decodeToString(charset: Charset): String =
-    Buffer().apply { write(this@decodeToString) }.readString(charset)
+@OptIn(UnsafeByteStringApi::class)
+internal actual fun ByteString.decodeToString(charset: Charset): String = String(unsafeBackingByteArray(), charset)
 
 internal actual fun String.toCharset(): Charset = Charset.forName(this)
