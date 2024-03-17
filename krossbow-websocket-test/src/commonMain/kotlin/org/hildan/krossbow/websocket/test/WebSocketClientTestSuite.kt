913d4aae4b2c2e1f8682783d05c4bb2af0a5c3ee
Joffrey Bion <joffrey.bion@gmail.com>
2024-03-17 12:53:12 +0100

    [test] Replace custom runSuspendingTest with runTest from kotlinx.coroutines.test
    We can avoid virtual time by using withContext with another dispatcher.

--- krossbow-websocket-test/src/commonMain/kotlin/org/hildan/krossbow/websocket/test/WebSocketClientTestSuite.kt
+++ krossbow-websocket-test/src/commonMain/kotlin/org/hildan/krossbow/websocket/test/WebSocketClientTestSuite.kt
@@ -1,8 +1,12 @@
 package org.hildan.krossbow.websocket.test
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.*
 import kotlinx.io.bytestring.*
 import org.hildan.krossbow.websocket.*
+import kotlin.coroutines.*
 import kotlin.test.*
+import kotlin.time.*
 import kotlin.time.Duration.Companion.seconds
 
 abstract class WebSocketClientTestSuite(
@@ -23,7 +27,7 @@ abstract class WebSocketClientTestSuite(
     }
 
     @Test
-    fun testConnectFailure_failsOnUnresolvedHost() = runSuspendingTest {
+    fun testConnectFailure_failsOnUnresolvedHost() = runTestRealTime {
         // Using the non-reified version because of the suspend inline function bug on JS platform
         // https://youtrack.jetbrains.com/issue/KT-37645
         val e = assertFailsWith(WebSocketConnectionException::class) {
@@ -35,110 +39,110 @@ abstract class WebSocketClientTestSuite(
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_200() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_200() = runTestRealTime {
         assertCorrectStatusReported(200) // 200 is not good for WS, should be 101
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_201_to_208() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_201_to_208() = runTestRealTime {
         assertCorrectStatusesReported(201..208) // 2xx is not good for WS, should be 101
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_301() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_301() = runTestRealTime {
         assertCorrectStatusReported(301)
     }
 
     // This range is broken down to avoid exceeding the test timeout and also helps to see issues with specific codes
     @Test
-    fun testConnectFailure_correctStatusCodeInException_302_to_305() = runSuspendingTest(timeout = 30.seconds) {
+    fun testConnectFailure_correctStatusCodeInException_302_to_305() = runTestRealTime {
         assertCorrectStatusesReported(302..305)
     }
     @Test
-    fun testConnectFailure_correctStatusCodeInException_306_to_308() = runSuspendingTest(timeout = 30.seconds) {
+    fun testConnectFailure_correctStatusCodeInException_306_to_308() = runTestRealTime {
         assertCorrectStatusesReported(306..308)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_400() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_400() = runTestRealTime {
         assertCorrectStatusReported(400)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_401() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_401() = runTestRealTime {
         assertCorrectStatusReported(401)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_402() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_402() = runTestRealTime {
         assertCorrectStatusReported(402)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_403() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_403() = runTestRealTime {
         assertCorrectStatusReported(403)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_404() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_404() = runTestRealTime {
         assertCorrectStatusReported(404)
     }
 
     // This range is broken down to avoid exceeding the test timeout and also helps to see issues with specific codes
     @Test
-    fun testConnectFailure_correctStatusCodeInException_405_to_408() = runSuspendingTest(timeout = 30.seconds) {
+    fun testConnectFailure_correctStatusCodeInException_405_to_408() = runTestRealTime {
         assertCorrectStatusesReported(405..408)
     }
     @Test
-    fun testConnectFailure_correctStatusCodeInException_409_to_412() = runSuspendingTest(timeout = 30.seconds) {
+    fun testConnectFailure_correctStatusCodeInException_409_to_412() = runTestRealTime {
         assertCorrectStatusesReported(409..412)
     }
     @Test
-    fun testConnectFailure_correctStatusCodeInException_413_to_415() = runSuspendingTest(timeout = 30.seconds) {
+    fun testConnectFailure_correctStatusCodeInException_413_to_415() = runTestRealTime {
         assertCorrectStatusesReported(413..415)
     }
     @Test
-    fun testConnectFailure_correctStatusCodeInException_416_to_418() = runSuspendingTest(timeout = 30.seconds) {
+    fun testConnectFailure_correctStatusCodeInException_416_to_418() = runTestRealTime {
         assertCorrectStatusesReported(416..418)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_500() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_500() = runTestRealTime {
         assertCorrectStatusReported(500)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_501() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_501() = runTestRealTime {
         assertCorrectStatusReported(501)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_502() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_502() = runTestRealTime {
         assertCorrectStatusReported(502)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_503() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_503() = runTestRealTime {
         assertCorrectStatusReported(503)
     }
 
     // This range is broken down to avoid exceeding the test timeout and also helps to see issues with specific codes
     @Test
-    fun testConnectFailure_correctStatusCodeInException_504_to_506() = runSuspendingTest(timeout = 30.seconds) {
+    fun testConnectFailure_correctStatusCodeInException_504_to_506() = runTestRealTime {
         assertCorrectStatusesReported(504..506)
     }
     @Test
-    fun testConnectFailure_correctStatusCodeInException_507_to_508() = runSuspendingTest(timeout = 30.seconds) {
+    fun testConnectFailure_correctStatusCodeInException_507_to_508() = runTestRealTime {
         assertCorrectStatusesReported(507..508)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_510() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_510() = runTestRealTime {
         assertCorrectStatusReported(510)
     }
 
     @Test
-    fun testConnectFailure_correctStatusCodeInException_511() = runSuspendingTest {
+    fun testConnectFailure_correctStatusCodeInException_511() = runTestRealTime {
         assertCorrectStatusReported(511)
     }
 
@@ -163,7 +167,7 @@ abstract class WebSocketClientTestSuite(
     }
 
     @Test
-    fun testHandshakeHeaders() = runSuspendingTest {
+    fun testHandshakeHeaders() = runTestRealTime {
         val connectWithTestHeaders = suspend {
             wsClient.connect(
                 url = "${testServerConfig.wsUrl}/echoHeaders",
@@ -187,7 +191,7 @@ abstract class WebSocketClientTestSuite(
     }
 
     @Test
-    fun testEchoText() = runSuspendingTest {
+    fun testEchoText() = runTestRealTime {
         val session = wsClient.connect(testServerConfig.wsUrl)
 
         try {
@@ -203,7 +207,7 @@ abstract class WebSocketClientTestSuite(
     }
 
     @Test
-    fun testEchoBinary() = runSuspendingTest {
+    fun testEchoBinary() = runTestRealTime {
         val session = wsClient.connect(testServerConfig.wsUrl)
 
         try {
@@ -217,5 +221,22 @@ abstract class WebSocketClientTestSuite(
 
         session.expectCloseFrame("after echo binary")
         session.expectNoMoreFrames("after echo binary CLOSE frame")
+    }
+}
+
+/**
+ * Behaves like [runTest] but without virtual time capabilities, so we can interact with real systems like the test
+ * server without instant timeouts.
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+private fun runTestRealTime(
+    context: CoroutineContext = EmptyCoroutineContext,
+    timeout: Duration = 60.seconds, // same default as runTest
+    block: suspend CoroutineScope.() -> Unit,
+) = runTest(timeout = timeout) {
+    // Switches to a regular dispatcher to avoid the virtual time from runTest.
+    // We also use limitedParallelism to avoid 
+    withContext(Dispatchers.Default.limitedParallelism(1) + context) {
+        block()
     }
 }
