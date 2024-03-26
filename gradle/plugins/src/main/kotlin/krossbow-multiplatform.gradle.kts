4a3e9cb54ddcdec45718aea84cf703c5fd924f80
Joffrey Bion <joffrey.bion@gmail.com>
2024-03-26 14:52:14 +0100

    WIP

--- gradle/plugins/src/main/kotlin/krossbow-multiplatform.gradle.kts
+++ gradle/plugins/src/main/kotlin/krossbow-multiplatform.gradle.kts
@@ -1,4 +1,7 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.*
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin.Companion.kotlinNodeJsExtension
+import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.*
 
 plugins {
     kotlin("multiplatform")
@@ -14,4 +17,18 @@ kotlin {
             }
         }
     }
+}
+
+// Drop this configuration when the Node.JS version in KGP will support wasm gc milestone 4
+// check it here:
+// https://github.com/JetBrains/kotlin/blob/master/libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/nodejs/NodeJsRootExtension.kt
+//extensions.configure<> {  }<NodeJsRootExtension>().nodeVersion {
+//    // canary nodejs that supports recent Wasm GC changes
+//    nodeVersion = "21.0.0-v8-canary202309167e82ab1fa2"
+//    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+//}
+
+// Drop this when node js version become stable
+tasks.withType<KotlinNpmInstallTask> {
+    args.add("--ignore-engines")
 }
