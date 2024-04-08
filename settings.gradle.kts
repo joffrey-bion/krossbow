c5770a9f06da58ae99cb1ee65c21a61f9622748c
Joffrey Bion <joffrey.bion@gmail.com>
2024-04-08 17:51:22 +0200

    [build] Remove refreshVersions as we're using dependabot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,5 @@
-import de.fayard.refreshVersions.core.DependencySelection
-import de.fayard.refreshVersions.core.StabilityLevel
-
 plugins {
     id("com.gradle.develocity") version "3.17"
-    id("de.fayard.refreshVersions") version "0.60.5"
 }
 
 rootProject.name = "krossbow"
@@ -39,16 +35,3 @@ develocity {
         uploadInBackground = false // background upload is bad for CI, and not critical for local runs
     }
 }
-
-refreshVersions {
-    // workaround for https://github.com/Splitties/refreshVersions/issues/640
-    versionsPropertiesFile = file("build/tmp/refreshVersions/versions.properties").apply { parentFile.mkdirs() }
-
-    rejectVersionIf {
-        candidate.stabilityLevel != StabilityLevel.Stable || isTyrus() || isOkHttpAlpha()
-    }
-}
-
-fun DependencySelection.isTyrus() = moduleId.name == "tyrus-standalone-client-jdk"
-
-fun DependencySelection.isOkHttpAlpha() = moduleId.name == "okhttp" && "-alpha" in candidate.value
