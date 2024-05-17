96ba6677925a987c733ddca39406c3f5e25a5cc5
Joffrey Bion <joffrey.bion@gmail.com>
2024-05-17 11:36:36 +0200

    Ktor/NodeJS now properly reports status codes in handshake errors in Kotlin >= 1.9.24

--- krossbow-websocket-ktor/src/jsTest/kotlin/org/hildan/krossbow/websocket/ktor/KtorJsWebSocketClientTest.kt
+++ krossbow-websocket-ktor/src/jsTest/kotlin/org/hildan/krossbow/websocket/ktor/KtorJsWebSocketClientTest.kt
@@ -3,12 +3,10 @@ package org.hildan.krossbow.websocket.ktor
 import io.ktor.client.engine.*
 import io.ktor.client.engine.js.*
 import org.hildan.krossbow.websocket.test.*
-import kotlin.time.Duration.Companion.milliseconds
 
 class KtorJsWebSocketClientTest : KtorClientTestSuite(
-    // JS node: error is too generic and doesn't differ per status code (ECONNREFUSED, unlike 'ws')
     // JS browser: cannot support status codes for security reasons
-    supportsStatusCodes = false,
+    supportsStatusCodes = currentJsPlatform() !is Platform.Js.Browser,
     // See https://youtrack.jetbrains.com/issue/KTOR-6970
     shouldTestNegotiatedSubprotocol = false,
 ) {
