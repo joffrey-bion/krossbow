46f85e81830e55008b1653c280d06d1cdb06a2ef
Joffrey Bion <joffrey.bion@gmail.com>
2024-08-26 01:54:28 +0200

    Add tests for withTransaction

--- krossbow-stomp-core/src/commonTest/kotlin/org/hildan/krossbow/stomp/StompSessionExtensionsTests.kt
+++ krossbow-stomp-core/src/commonTest/kotlin/org/hildan/krossbow/stomp/StompSessionExtensionsTests.kt
@@ -41,4 +41,73 @@ class StompSessionExtensionsTests {
         }
         assertTrue(wsSession.closed, "The web socket session should be closed after the use block")
     }
+
+    @Test
+    fun withTransaction_commitsIfSuccessful() = runTest {
+        val (wsSession, stompSession) = connectWithMocks()
+
+        launch {
+            stompSession.withTransaction { id ->
+                sendText("/dest", "Transaction: $id")
+            }
+            stompSession.disconnect()
+        }
+        val beginFrame = wsSession.awaitBeginFrameAndSimulateCompletion()
+        val transactionId = beginFrame.headers.transaction
+
+        val sendFrame = wsSession.awaitSendFrameAndSimulateCompletion()
+        assertEquals(transactionId, sendFrame.headers.transaction)
+
+        val commitFrame = wsSession.awaitCommitFrameAndSimulateCompletion()
+        assertEquals(transactionId, commitFrame.headers.transaction)
+
+        wsSession.awaitDisconnectFrameAndSimulateCompletion()
+        wsSession.expectClose()
+    }
+
+    @Test
+    fun withTransaction_abortsInCaseOfException() = runTest {
+        val (wsSession, stompSession) = connectWithMocks()
+
+        launch {
+            runCatching {
+                stompSession.withTransaction { id ->
+                    sendText("/dest", "Transaction: $id")
+                    emptyList<Int>().first() // this fails
+                }
+            }
+            stompSession.disconnect()
+        }
+        val beginFrame = wsSession.awaitBeginFrameAndSimulateCompletion()
+        val transactionId = beginFrame.headers.transaction
+
+        val sendFrame = wsSession.awaitSendFrameAndSimulateCompletion()
+        assertEquals(transactionId, sendFrame.headers.transaction)
+        assertEquals("Transaction: $transactionId", sendFrame.bodyAsText)
+
+        val abortFrame = wsSession.awaitAbortFrameAndSimulateCompletion()
+        assertEquals(transactionId, abortFrame.headers.transaction)
+
+        wsSession.awaitDisconnectFrameAndSimulateCompletion()
+        wsSession.expectClose()
+    }
+
+    @Test
+    fun withTransaction_abortsWithSuppressedException() = runTest {
+        class MyAbortException : Exception("exception during abort")
+
+        val stompSession = object : NoopStompSession() {
+            override suspend fun abort(transactionId: String) {
+                throw MyAbortException()
+            }
+        }
+
+        val ex = assertFailsWith(NoSuchElementException::class) {
+            stompSession.withTransaction {
+                emptyList<Int>().first() // this fails
+            }
+        }
+        assertEquals(1, ex.suppressedExceptions.size)
+        assertIs<MyAbortException>(ex.suppressedExceptions.single())
+    }
 }
