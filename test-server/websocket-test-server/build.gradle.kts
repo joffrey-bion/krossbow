56a8c578720c6f99e72ea8e264251e050c153879
Joffrey Bion <joffrey.bion@gmail.com>
2024-09-22 18:35:37 +0200

    Align the Kotlin version of the test server so it matches Gradle
    This prepares for Kotlin 2.1.

--- test-server/websocket-test-server/build.gradle.kts
+++ test-server/websocket-test-server/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
 }
@@ -5,4 +7,12 @@ plugins {
 dependencies {
     implementation(libs.java.websocket)
     implementation(libs.kotlinx.coroutines.core)
+}
+
+kotlin {
+    compilerOptions {
+        // this is to match the embedded Kotlin version when used in Gradle build scripts
+        languageVersion.set(KotlinVersion.KOTLIN_1_9)
+        apiVersion.set(KotlinVersion.KOTLIN_1_9)
+    }
 }
